5797e6514da3a58cc2f022e99f03eea74fb5f241
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-10-14 16:05:29 +0800

    Fix importer expr check

--- importer.sh
+++ importer.sh
@@ -4,15 +4,15 @@ NEBULA_IMPORTER=nebula-importer-1.0.0-beta.jar
 GRAPH_ADDR=127.0.0.1:3699
 NAMESPACE=wb
 
-if [ -z "$1" ];then
+if [ -n "$1" ];then
   NEBULA_IMPORTER=$1
 fi
 
-if [ -z "$2" ];then
+if [ -n "$2" ];then
   GRAPH_ADDR=$2
 fi
 
-if [ -z "$3" ];then
+if [ -n "$3" ];then
   NAMESPACE=$3
 fi
 
@@ -22,5 +22,5 @@ java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema db -
 java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema start -u user -p password -t edge --file start.csv --column start_time,end_time
 java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema end -u user -p password -t edge --file end.csv --column start_time,end_time
 java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema inherit -u user -p password -t edge --file inherit.csv --column job_id,start_time,end_time
-java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema contain -u user -p password -t edge --file contain.csv
-java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema reverse_contain -u user -p password -t edge --file reverse_contain.csv
+java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema contain -u user -p password -t edge --file contain.csv --column ""
+java -jar $NEBULA_IMPORTER --address $GRAPH_ADDR --name $NAMESPACE --schema reverse_contain -u user -p password -t edge --file reverse_contain.csv --column ""
